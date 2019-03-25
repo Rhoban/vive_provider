72cccf781d26878a30ad0d2307c9b68fcbf5c633
apirrone <awanegenebayo@gmail.com>
2019-03-25 18:55:22 +0100

    zmq

--- vive_server.py
+++ vive_server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from vive_pb2 import *
+import zmq
 
 import socket
 import time
@@ -10,11 +11,17 @@ from vive_provider import *
 
 vp = Vive_provider()
 
+context = zmq.Context()
+socket = context.socket(zmq.PUB)
+socket.bind("tcp://*:4040")
 
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-server.settimeout(0.2)
-server.bind(("", 44444))
+
+
+
+# server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+# server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+# server.settimeout(0.2)
+# server.bind(("", 44444))
 
 pb_msg = GlobalMsg()
 i = 0
@@ -26,8 +33,10 @@ while True:
     
     # temporary, converting to bytes
     trackersInfos = pb_msg.SerializeToString()
+    socket.send(trackersInfos)
+    print(str(i))
     
-    bytes_sent = server.sendto(trackersInfos, ('<broadcast>', 37020))
-    print("bytes_sent: {}".format(bytes_sent))
+    # bytes_sent = server.sendto(trackersInfos, ('<broadcast>', 37020))
+    # print("bytes_sent: {}".format(bytes_sent))
     time.sleep(0.01)
     i+=1
