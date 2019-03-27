5ed665366b587bb1d6507a3bfbdc0737b7d4d272
Gregwar <g.passault@gmail.com>
2019-03-27 17:45:43 +0100

    Displaying RPY of devices

--- vive_server.py
+++ vive_server.py
@@ -10,7 +10,8 @@ from vive_provider import *
 
 vp = Vive_provider()
 
-
+addr = '<broadcast>'
+addr = '10.0.0.255'
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 server.settimeout(0.2)
@@ -31,10 +32,13 @@ while True:
     print('* Tracking %d devices' % len(trackers['trackers']))
     for id in trackers['trackers']:
         p = trackers['trackers'][id]['pose']
-        print('- %s (%g, %g, %g)' % (id, p[0], p[1], p[2]))
+        rpy = np.array(convert_to_euler(trackers['trackers'][id]['pose_matrix']))*180.0/math.pi
+        print('- %s' % (id))
+        print('  - x: %g, y: %g, z: %g' % (p[0], p[1], p[2]))
+        print('  - roll: %g, pitch: %f, yaw: %g' % tuple(rpy))
     print()
     
-    bytes_sent = server.sendto(trackersInfos, ('<broadcast>', 37020))
+    bytes_sent = server.sendto(trackersInfos, (addr, 37020))
     # print("bytes_sent: {}".format(bytes_sent))
     time.sleep(0.01)
     i+=1
